2c3d3a4b0100552eb7901928794f9557aa7b2f7d
Andrew Toms <andrew@anglersatlas.com>
2015-11-23 09:06:48 -0800

    Initial Commit of PDF Tester App.

---
+++ app/src/main/java/com/mapjungle/pdftester/MainActivity.java
@@ -0,0 +1,43 @@
+package com.mapjungle.pdftester;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView mWebView=new WebView(this);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://media.wizards.com/2015/downloads/dnd/02_UA_Underdark_Characters.pdf");
+        setContentView(mWebView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
